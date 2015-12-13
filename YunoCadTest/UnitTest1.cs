c175e17dc7b7faa3ba2f8e1d83363daf57964a05
Yuhki Ogasawara <y121516@gmail.com>
2015-12-13 19:49:43 +0900

    ユニットテストプロジェクトを追加した。

---
+++ YunoCadTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YunoCad;
+
+namespace YunoCadTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var a = new Class1();
+        }
+    }
+}
