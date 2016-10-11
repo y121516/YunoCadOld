9c148224dca01fd15cad359360d1883c66714085
Yuhki Ogasawara <y121516@gmail.com>
2016-10-11 13:04:59 +0900

    ConversingSession.KillInteractiveCommandメソッドを追加。

--- YunoCad/ConversingSession.cs
+++ YunoCad/ConversingSession.cs
@@ -46,6 +46,8 @@ namespace Yuno.Cad
                 return v;
             }
         }
+
+        void KillInteractiveCommand() => MC.KillInteractiveCmd();
     }
 
     public struct SystemVersion
